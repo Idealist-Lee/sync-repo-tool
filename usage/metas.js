4fa2cb2061d11d25f6f8d27ea51b505017d46d65
Xavi Lee <awxiaoxian2020@163.com>
2024-02-08 09:08:49 +0800

    update usages

--- usage/metas.js
+++ usage/metas.js
@@ -1,12 +1,24 @@
-export default function (){
-    return [{
-         originalRepoOwner: "microsoft",
-         originalRepoName: "TypeScript-Website",
-         originalRepoDefaultBranch: "v2",
-         needSyncRepoOwner: "ts-zh-docs",
-         needSyncRepoName: "TypeScript-Website",
-         needSyncRepoBranch: "v2",
-         syncerUsername: "SyncDocsBot",
-         syncerEmail: "tsdocs@126.com"
-    }]
+export default function () {
+  return [
+    {
+      originalRepoOwner: "microsoft",
+      originalRepoName: "TypeScript-Website",
+      originalRepoDefaultBranch: "v2",
+      needSyncRepoOwner: "ts-zh-docs",
+      needSyncRepoName: "TypeScript-Website",
+      needSyncRepoBranch: "v2",
+      syncerUsername: "SyncDocsBot",
+      syncerEmail: "tsdocs@126.com",
+    },
+    {
+      originalRepoOwner: "microsoft",
+      originalRepoName: "TypeScript-Website",
+      originalRepoDefaultBranch: "v2",
+      needSyncRepoOwner: "ts-zh-docs",
+      needSyncRepoName: "tslang.org.cn",
+      needSyncRepoBranch: "v2",
+      syncerUsername: "SyncDocsBot",
+      syncerEmail: "tsdocs@126.com",
+    },
+  ];
 }
